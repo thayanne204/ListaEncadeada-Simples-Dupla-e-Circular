64b8b70ac6d574a792563e882b9351fdd19d8afd
thayanne204 <oliveirathayanne438@gmail.com>
2025-10-25 23:49:40 -0300

    Create NodeSimples class for linked list nodes

---
+++ NodeSimples.java
@@ -0,0 +1,9 @@
+public class NodeSimples {
+    public int data;
+    public NodeSimples next;
+    
+    public NodeSimples(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
