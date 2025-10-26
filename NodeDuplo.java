555ac9815e0d2bd66117eaabc34b0cdb188701de
thayanne204 <oliveirathayanne438@gmail.com>
2025-10-25 23:50:14 -0300

    Create NodeDuplo.java

---
+++ NodeDuplo.java
@@ -0,0 +1,11 @@
+public class NodeDuplo {
+    public int data;
+    public NodeDuplo next;
+    public NodeDuplo previous;
+    
+    public NodeDuplo(int data) {
+        this.data = data;
+        this.next = null;
+        this.previous = null;
+    }
+}
