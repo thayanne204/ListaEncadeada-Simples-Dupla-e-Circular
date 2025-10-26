d140814376d227d78f071bd965e5f8d18a4ab6e7
thayanne204 <oliveirathayanne438@gmail.com>
2025-10-25 23:50:50 -0300

    Create NodeCircular class for circular linked list

---
+++ NodeCircular.java
@@ -0,0 +1,9 @@
+public class NodeCircular {
+    public int data;
+    public NodeCircular next;
+    
+    public NodeCircular(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
